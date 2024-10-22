31be7715ba15708bbd9e5f92fd864b1eeb7b30f1
hohlovscky2017 <89722647+hohlovscky2017@users.noreply.github.com>
2024-10-22 15:11:49 +0300

    Добавьте файлы проекта.

---
+++ POSales/DBConnect.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POSales
+{
+    class DBConnect
+    {
+        private string con;
+        public string myConnection()
+        {
+            con = @"Data Source=DESKTOP-E3LB6O2\SQLEXPRESS;Initial Catalog=DBPOSale;Integrated Security=True;Encrypt=False";
+            return con;
+        }
+    }
+}
---
+++ POSales/MainForm.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace POSales
+{
+    public partial class MainForm : Form
+    {
+        SqlConnection cn = new SqlConnection();
+        SqlCommand cm = new SqlCommand();
+        DBConnect dbcon = new DBConnect();
+        public MainForm()
+        {
+            InitializeComponent();
+            customizeDezing();
+            cn = new SqlConnection(dbcon.myConnection());
+            cn.Open();
+        }
+
+        #region panelSlide
+        private void customizeDezing()
+        {
+            panelSubProduct.Visible = false;
+            panelSubRecord.Visible = false;
+            panelSubStock.Visible = false;
+            panelSubSetting.Visible = false;
+        }
+
+        private void HideSubmenu()
+        {
+            if (panelSubProduct.Visible == true)
+                panelSubProduct.Visible = false;
+            if(panelSubRecord.Visible == true)
+                panelSubRecord.Visible = false;
+            if(panelSubSetting.Visible == true)
+                panelSubSetting.Visible = false;
+            if(panelSubStock.Visible == true)
+                panelSubStock.Visible = false;
+        }
+
+        private void showSubmenu(Panel submenu)
+        {
+            if(submenu.Visible == false)
+            {
+                HideSubmenu();
+                submenu.Visible = true;
+            }
+            else
+                submenu.Visible = false;
+        }
+        #endregion panelSlide
+
+        private void btnDashboard_Click(object sender, EventArgs e)
+        {
+            HideSubmenu();
+        }
+
+        private void btnProduct_Click(object sender, EventArgs e)
+        {
+            showSubmenu(panelSubProduct);
+        }
+
+        private void btnProductList_Click(object sender, EventArgs e)
+        {
+            HideSubmenu();
+        }
+
+        private void btnCategory_Click(object sender, EventArgs e)
+        {
+            HideSubmenu();
+        }
+
+        private void btnBrand_Click(object sender, EventArgs e)
+        {
+            HideSubmenu();
+        }
+
+        private void btnInStock_Click(object sender, EventArgs e)
+        {
+            showSubmenu(panelSubStock);
+        }
+
+        private void btnStockEntry_Click(object sender, EventArgs e)
+        {
+            HideSubmenu();
+        }
+
+        private void btnStockAdjastment_Click(object sender, EventArgs e)
+        {
+            HideSubmenu();
+        }
+
+        private void btnSupplier_Click(object sender, EventArgs e)
+        {
+            HideSubmenu();
+        }
+
+        private void btnRecord_Click(object sender, EventArgs e)
+        {
+            showSubmenu(panelSubRecord);
+        }
+
+        private void btnSaleHistory_Click(object sender, EventArgs e)
+        {
+            HideSubmenu();
+        }
+
+        private void btnPosRecord_Click(object sender, EventArgs e)
+        {
+            HideSubmenu();
+        }
+
+        private void btnSetting_Click(object sender, EventArgs e)
+        {
+            showSubmenu(panelSubSetting);
+        }
+
+        private void btnUser_Click(object sender, EventArgs e)
+        {
+            HideSubmenu();
+        }
+
+        private void btnStore_Click(object sender, EventArgs e)
+        {
+            HideSubmenu();
+        }
+
+        private void btnLogout_Click(object sender, EventArgs e)
+        {
+            HideSubmenu();
+        }
+    }
+}
